ab5a429ccb3040c56e1c6aed9140d577c5057984
alparslanavci <alparslanavci@gmail.com>
2020-10-15 14:30:35 +0100

    Testing upgrades

--- hazelcast.rb
+++ hazelcast.rb
@@ -1,8 +1,8 @@
 class Hazelcast < Formula
   desc "Tool to run Hazelcast IMDG member instances locally"
   homepage "https://github.com/hazelcast/hazelcast-command-line"
-  url "https://github.com/alparslanavci/hazelcast-command-line/releases/download/v4.2020.10/hazelcast-4.1-BETA-1.tar.gz"
-  sha256 "7cc639a3fb39553b2c2f1bc508711029fac01a59c2f654fa359e0693ce351ad3"
+  url "https://github.com/alparslanavci/homebrew-hz/releases/download/v4.2020.10/hazelcast-4.1-BETA-1.tar.gz"
+  sha256 "5d10ec3f94f4bffc1ad2b4aa7d56c7fe3e2b9fecee1ff9b1db946c0e3834d71b"
 
   bottle :unneeded
 
